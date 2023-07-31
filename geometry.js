6a40d219775aa5c88283685a71e4bb7b55a06c20
Barshon <barshonweb@gmail.com>
2023-07-31 10:11:26 +0600

    input taking functionality with validation

--- geometry.js
+++ geometry.js
@@ -2,3 +2,33 @@
 document.getElementById("blog-button-section").addEventListener("click",function(event){
     location.href="blog.html";
 })
+
+// single input taking funvtion 
+function getValue(inputId){
+    const inputElement=document.getElementById(inputId);
+    const inputString=inputElement.value;
+    inputElement.value="";
+    const inputNumber=parseFloat(inputString);
+    return inputNumber;
+}
+
+// both input taking function 
+function getValuesfromInput(input1Id,input2Id){
+    const value1=getValue(input1Id);
+    const value2=getValue(input2Id);
+    if(isNaN(value1) || isNaN(value2)){
+        alert("Please enter number");
+    }
+    console.log(value1);
+    console.log(value2);
+}
+
+// calculation button click event function 
+function clickCalc(buttonId,in1,in2){
+document.getElementById(buttonId).addEventListener("click",function(){
+    getValuesfromInput(in1,in2);
+})
+}
+
+// function call for triangle calculation 
+clickCalc("triangle-calc","base-input","height-input");
